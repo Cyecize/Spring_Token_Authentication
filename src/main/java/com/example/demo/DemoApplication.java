c5497bfa56ade83affb33444d21e85d2a2d882c8
cyecize <ceci2205@abv.bg>
2019-06-11 12:02:47 +0300

    Added basic configurations and dependencies.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class DemoApplication {
+
+    @Value("${server.port}")
+    private int port;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void onAppReady() {
+        System.out.println(String.format("Listening: http://localhost:%s", this.port));
+    }
 }
---
+++ src/main/java/com/example/demo/config/BeanConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Gson gson() {
+        return new Gson();
+    }
+}
